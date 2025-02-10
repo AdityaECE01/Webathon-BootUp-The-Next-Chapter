fa1d53192485551715e735db4837db6739d0dd41
AdityaECE01 <ak5143660@gmail.com>
2025-02-10 20:07:23 +0530

    Create 02.js

---
+++ Week-2/Task-1/02.js
@@ -0,0 +1,15 @@
+Convert Temperature (Celsius to Fahrenheit).
+
+
+function celsiusToFahrenheit(celsius) 
+{
+    return (celsius * 9/5) + 32;
+}
+
+
+let tempCelsius = 25; 
+let tempFahrenheit = celsiusToFahrenheit(tempCelsius);
+
+
+console.log(`${tempCelsius}°C is equal to ${tempFahrenheit.toFixed(2)}°F`);
+  
